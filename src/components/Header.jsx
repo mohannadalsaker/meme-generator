0c0ef9465aa71cf2d656931ac0845233eddca42e
Mohannad Saker <mhndsaker@gmail.com>
2023-09-20 21:53:13 +0300

    my first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+export default function Header() {
+  return (
+    <header className="header">
+      <img src="./troll-face.png" className="header--image" alt="troll-face" />
+      <h2 className="header--title">Meme Generator</h2>
+      <h4 className="header--project">
+        My Project By <b>Mohannad</b>
+      </h4>
+    </header>
+  );
+}
